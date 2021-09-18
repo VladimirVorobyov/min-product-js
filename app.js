00ba3c9b13267b6951d08804d19a0d5f4b41210b
Vova Vorobiev <vovabandit1945@gmail.com>
2021-09-18 13:47:42 +0300

    lesson4

--- app.js
+++ app.js
@@ -1,44 +1,20 @@
-const down = document.querySelector('.down-button');
-const up = document.querySelector('.up-button');
-const slider = document.querySelector('.sidebar');
-const mainSlide = document.querySelector('.main-slide');
-const numberSlede = mainSlide.querySelectorAll('div').length;
-const container = document.querySelector('.container');
+const board = document.querySelector('#board');
+const colors = [ '#2c0bb3','#5806c4','#2db4ed','#2deded',
+'#22e3cc','#22e39c', '#0cc449', '#c45506','#d14330'];
+const SQUARES = 500;
 
-slider.style.top = `-${(numberSlede-1)*100}vh`;
-let sumSlide = 0;
-
-
-down.addEventListener('click', ()=>addSlide('down'));
-up.addEventListener('click', ()=> addSlide('up'));
-
-document.addEventListener('keydown', (e)=>{
-  if(e.key === 'ArrowUp'){
-    addSlide('up')
-  }else if(e.key === 'ArrowDown'){
-    addSlide('down')
-  }
-});
-
-
-function addSlide(def){
-  if(def === 'up'){
-    sumSlide ++; 
-    if(sumSlide === numberSlede){
-      sumSlide = 0;
-    }
-  }else if(def === 'down'){
-    sumSlide --;
-    if(sumSlide<0){
-      sumSlide = numberSlede -1; 
-    }
-  }
-
-  const height = container.clientHeight; 
-  mainSlide.style.transform = `translateY(-${sumSlide * height}px)`;
-  slider.style.transform = `translateY(${sumSlide * height}px)`
-
-
-}
-
-
+for (let i = 0; i<SQUARES; i++ ) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  board.append(square);
+  
+  square.addEventListener('mouseover', ()=>{
+    let color = colors[Math.floor(Math.random() * colors.length)];
+    square.style.backgroundColor = color ;
+    square.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+  })
+  square.addEventListener('mouseout', ()=>{
+    square.style.backgroundColor = '#3b3b3b';
+    square.style.boxShadow = `0 0 2px #3b3b3b`
+  })
+};
